d4176c89cf054a59d16d4aeef2dc60f06824b38d
Ahmad Awais <mrahmadawais@gmail.com>
2017-12-10 07:02:01 +0500

    FIX: Basic 01 💯

--- block/01-basic/block.js
+++ block/01-basic/block.js
@@ -25,17 +25,17 @@
 	 * @return {?WPBlock}          The block, if it has been successfully
 	 *                             registered; otherwise `undefined`.
 	 */
-	registerBlockType( 'gb/example-01-basic', { // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	registerBlockType( 'gb/basic-01', { // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 		title: __( 'Basic', 'GB' ), // Block title.
 		icon: 'shield-alt', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 		category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 
 		// The "edit" property must be a valid function.
 		edit: function( props ) {
-			// Creates a <p class='wp-block-gb-01-basic'></p>.
+			// Creates a <p class='wp-block-gb-basic-01'></p>.
 			return el(
 				'p', // Tag type.
-				{ className: props.className }, // The class="wp-block-gb-01-basic" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
+				{ className: props.className }, // The class="wp-block-gb-basic-01" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
 				'Hello World! — from the editor (01 Basic Block).' // Content inside the tag.
 			);
 		},
@@ -44,7 +44,7 @@
 		save: function( props ) {
 			return el(
 				'p', // Tag type.
-				{ className: props.className }, // The class="wp-block-gb-01-basic" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
+				{ className: props.className }, // The class="wp-block-gb-basic-01" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
 				'Hello World! — from the frontend (01 Basic Block).' // Content inside the tag.
 			);
 		},
